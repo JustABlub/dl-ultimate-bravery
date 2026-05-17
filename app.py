3197670b24bc1736b12578ed35de9dad1f6935ef
JustABlub <minhanpark882@gmail.com>
2026-05-16 23:32:09 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+import random
+import json
+
+with open('./resources/characters.json') as f:
+    characters = json.load(f)
+with open("./resources/items.json") as f:
+    item_page = json.load(f)
+
+ITEM_TYPE = ["Spirit", "Vitality", "Weapon"]
+
+def generate_draft():
+    return random.sample(population=characters, k=4)
+
+def generate_items(build_type, game_phase, k, used=None):
+    if used is None:
+        used = set()
+    if build_type == "Spirit":
+        item_bias = [70, 15, 15]
+    elif build_type == "Vitality":
+        item_bias = [15, 70, 15]
+    elif build_type == "Weapon":
+        item_bias = [15, 15, 70]
+
+    if game_phase == "early":
+        soul_bias = [50, 30, 15, 5, 0]
+    elif game_phase == "mid":
+        soul_bias = [0, 20, 50, 30, 0]
+    elif game_phase == "late":
+        soul_bias = [0, 0, 0, 1, 0]
+
+    types = list(item_page.keys())
+    build_page = []
+    for _ in range(k):
+        while True:
+            item_type = random.choices(types, weights=item_bias, k=1)[0]
+            tiers = list(item_page[item_type].keys())
+            soul = random.choices(tiers, weights=soul_bias, k=1)[0]
+            pool = [i for i in item_page[item_type][soul] if i not in used]
+            if pool:
+                item = random.choice(pool)
+                break
+        used.add(item)
+        build_page.append(item)
+    return build_page
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    build_bias = random.choice(ITEM_TYPE)
+    used = set()
+    return render_template(
+        'index.html',
+        draft=generate_draft(),
+        early=generate_items(build_bias, "early", 4, used),
+        mid=generate_items(build_bias, "mid", 4, used),
+        late=generate_items(build_bias, "late", 4, used),
+    )
---
+++ update_items.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+itempage = "https://deadlock.wiki/Items"
+
+page = requests.get(itempage)
+soup = BeautifulSoup(page.text, "html.parser")
+
+table = soup.find("table", {"class": "navbox"})
+trs = table.find_all("tr")[1:]
+
+master_list = {}
+for item_type_tr in trs:
+    category_list = {}
+    try:
+        title_span = item_type_tr.find("span", {"class": "item-nav-category-text"})
+        item_table = item_type_tr.find("table", {"class": "navbox-subgroup"})
+
+        soul_categories = item_table.find_all("tr")[::2]
+        for tr in soul_categories:
+            try:
+                item_soul_value = tr.find("b").get_text()
+                unparsed_items = tr.find_all("span", {"class": "item-link-wrapper"})
+
+                temp_arr = []
+                for item in unparsed_items:
+                    temp_arr.append(item.get_text()[1:])
+                category_list[item_soul_value] = temp_arr
+            except:
+                pass
+        master_list[title_span.get_text()] =  category_list
+    except:
+        pass
+
+with open('items.json', 'w') as fp:
+    json.dump(master_list, fp, sort_keys=False, indent=4)
